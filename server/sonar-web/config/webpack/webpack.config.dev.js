4a4acabebea3956f703b0d71c88d9ed89557ed60
Stas Vilchik <vilchiks@gmail.com>
2017-05-28 14:39:43 +0200

    wip

--- server/sonar-web/config/webpack/webpack.config.dev.js
+++ server/sonar-web/config/webpack/webpack.config.dev.js
@@ -258,6 +258,7 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', minChunks: Infinity }),
     // Makes some environment variables available in index.html.
     // The public URL is available as %PUBLIC_URL% in index.html, e.g.:
     // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico">
